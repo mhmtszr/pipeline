d757867c5c8d15d25722ae1ae49cc4351f273250
mhmtszr <m.szr98@gmail.com>
2026-03-19 13:54:28 +0300

    update: github workflows

--- pipeline.go
+++ pipeline.go
@@ -1,9 +1,12 @@
+// Package pipeline provides a generic, function-based pipeline builder.
 package pipeline
 
+// Pipeline is a compiled chain of steps ready to execute.
 type Pipeline[K any] struct {
 	handler func(K) error
 }
 
+// Execute runs the pipeline with the given context.
 func (p Pipeline[K]) Execute(ctx K) error {
 	return p.handler(ctx)
 }
--- pipeline_builder.go
+++ pipeline_builder.go
@@ -2,19 +2,23 @@ package pipeline
 
 import "golang.org/x/sync/errgroup"
 
+// Builder constructs a pipeline by chaining steps.
 type Builder[K any] struct {
 	steps []StepFunc[K]
 }
 
+// NewBuilder creates a new pipeline builder.
 func NewBuilder[K any]() *Builder[K] {
 	return &Builder[K]{}
 }
 
+// Use adds a step to the pipeline.
 func (b *Builder[K]) Use(step StepFunc[K]) *Builder[K] {
 	b.steps = append(b.steps, step)
 	return b
 }
 
+// UseConcurrent adds a set of functions that run concurrently as a single step.
 func (b *Builder[K]) UseConcurrent(steps ...func(K) error) *Builder[K] {
 	b.steps = append(b.steps, func(ctx K, next func(K) error) error {
 		var eg errgroup.Group
@@ -29,6 +33,7 @@ func (b *Builder[K]) UseConcurrent(steps ...func(K) error) *Builder[K] {
 	return b
 }
 
+// UseConditional adds a conditional step that branches based on cond.
 func (b *Builder[K]) UseConditional(
 	cond func(K) bool,
 	ifTrue []StepFunc[K],
@@ -49,12 +54,13 @@ func (b *Builder[K]) UseConditional(
 	return b
 }
 
+// Build compiles the steps into an executable Pipeline.
 func (b *Builder[K]) Build() Pipeline[K] {
 	return Pipeline[K]{handler: buildChain(b.steps)}
 }
 
 func buildChain[K any](steps []StepFunc[K]) func(K) error {
-	var h = func(K) error { return nil }
+	h := func(K) error { return nil }
 	for i := len(steps) - 1; i >= 0; i-- {
 		next := h
 		step := steps[i]
